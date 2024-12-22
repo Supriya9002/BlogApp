c420ad6e906a9df1beea9ea4ca609f272471cced
Supriya Haldar <112514582+Supriya9002@users.noreply.github.com>
2024-12-22 21:53:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,10 @@ import connectUsingMongoose from "./src/config/db.js";
 import blogRouter from "./src/Blog/blog.routes.js";
 import bodyParser from "body-parser";
 import cors from "cors";
-import path from "path";
 
 // server
 const server = express();
-const _dirname = path.resolve();
+
 // All Middleware
 server.use(bodyParser.json());
 server.use(cors());
@@ -21,13 +20,9 @@ server.get("/", (req, res) => {
   res.send("Welcome To Bloger App");
 });
 
-server.use(express.static(path.join(_dirname, "/frontend/build")))
-server.get('*', (req, res)=>{
-  res.sendFile(path.resolve(_dirname, "frontend", "build", "index.html"))
-})
 // console.log("process.env.Port",process.env.Port)
 // Port
-console.log("process.env.Port", process.env.Port)
+// console.log("process.env.Port", process.env.Port)
 server.listen(process.env.Port || 3000, () => {
   connectUsingMongoose();
   console.log(`Server Listen is port ${process.env.Port}`);
